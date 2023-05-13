eea2887ffd4defa94fd7e465e8bab37ede4502d2
deejaydhiz <djbanire@gmail.com>
2023-05-13 04:13:34 -0400

    Temporary changes

--- final_project/machinetranslation/ibmsample.py
+++ final_project/machinetranslation/ibmsample.py
@@ -11,8 +11,11 @@ language_translator = LanguageTranslatorV3(
 language_translator.set_service_url('https://api.us-east.language-translator.watson.cloud.ibm.com/instances/72ff0e6d-75bc-4148-a1e7-dde6b8db2d34')
 
 translation = language_translator.translate(
-    text=['Hello', 'how', 'are', 'you', 'today?'],
+    text=['ten', 'how', 'are', 'you', 'today?'],
     model_id='en-es').get_result()
 print(json.dumps(translation, indent=2, ensure_ascii=False))
-x = translation["translations"]
-print (x)
+x = []
+for i in translation:
+    for j in 
+    x = translation["translations"][0:]
+print (x.values())
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -7,8 +7,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-apikey = os.environ['apikey']
-url = os.environ['url']
+apikey = os.environ['Uom88dgP12KqnUbtA73RzAWA1FKvZfqsAdZsd1VczV2b']
+url = os.environ['https://api.us-east.language-translator.watson.cloud.ibm.com/instances/72ff0e6d-75bc-4148-a1e7-dde6b8db2d34']
 
 authenticator = IAMAuthenticator('apikey')
 language_translator = LanguageTranslatorV3(
