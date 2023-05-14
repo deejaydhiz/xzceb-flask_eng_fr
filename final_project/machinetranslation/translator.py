9394a7f2a172780a0dce41139d63acca49f2f898
deejaydhiz <djbanire@gmail.com>
2023-05-14 02:34:50 -0400

    Temporary changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -10,21 +10,21 @@ load_dotenv()
 apikey = os.environ['apikey']
 url = os.environ['url']
 
-authenticator = IAMAuthenticator('apikey')
+authenticator = IAMAuthenticator('Uom88dgP12KqnUbtA73RzAWA1FKvZfqsAdZsd1VczV2b')
 language_translator = LanguageTranslatorV3(
     version='2018-05-01',
     authenticator=authenticator
 )
 
-language_translator.set_service_url('url')
+language_translator.set_service_url('https://api.us-east.language-translator.watson.cloud.ibm.com/instances/72ff0e6d-75bc-4148-a1e7-dde6b8db2d34')
 
-#function to translate English to French 
+#function to translate English to French
 def english_to_french(english_text):
     '''write the code here'''
     translation = language_translator.translate(
-    englishText,
+    english_text,
     model_id='en-fr').get_result()
-    french_text = translation("translations")
+    french_text = translation["translations"][0]["translation"]
     return french_text
 
 #function to translate French to English
@@ -33,5 +33,5 @@ def french_to_english(french_text):
     translation = language_translator.translate(
     french_text,
     model_id='fr-en').get_result()
-    french_text = translation("translations")
+    english_text = translation["translations"][0]["translation"]
     return english_text
