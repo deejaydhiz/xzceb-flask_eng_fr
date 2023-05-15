14dbcbbd11774233eeff79959ba20467402e531e
deejaydhiz <djbanire@gmail.com>
2023-05-15 14:53:23 -0400

    Final changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -9,13 +9,13 @@ load_dotenv()
 apikey = os.environ['apikey']
 url = os.environ['url']
 
-authenticator = IAMAuthenticator(apikey)
+authenticator = IAMAuthenticator('Uom88dgP12KqnUbtA73RzAWA1FKvZfqsAdZsd1VczV2b')
 language_translator = LanguageTranslatorV3(
     version='2018-05-01',
     authenticator=authenticator
 )
 
-language_translator.set_service_url(url)
+language_translator.set_service_url('https://api.us-east.language-translator.watson.cloud.ibm.com/instances/72ff0e6d-75bc-4148-a1e7-dde6b8db2d34')
 
 #function to translate English to French
 def english_to_french(english_text):
